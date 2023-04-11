db703dfe128fe51aa7d9dce28cb54f93e8692619
Mina Yossry <mina.yossry4@gmail.com>
2023-04-11 16:19:28 +0200

    use Fake API

--- src/app/Services/Products/products.service.ts
+++ src/app/Services/Products/products.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class ProductsService {
   constructor(private readonly httpClient: HttpClient) {}
 
-  private readonly URL = 'http://localhost:3000/products'; //API
+  private readonly URL = 'https://fakestoreapi.com/products/'; //API
 
   GetAllProducts() {
     return this.httpClient.get(this.URL);
