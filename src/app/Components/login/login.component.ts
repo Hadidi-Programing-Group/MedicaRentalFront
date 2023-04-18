26ad64e612f5f810f600ed6832d09737c597107d
HebaTullah Abdelazem <57576774+Hebaallah61@users.noreply.github.com>
2023-04-18 17:12:53 +0200

    Login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/Services/Login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +11,44 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup; // Declare loginForm as FormGroup
+  loginForm: FormGroup; 
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private loginservies: LoginService, private router:Router) 
+  {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      email: [
+        '',
+      [
+        Validators.required,
+       
+      ],
+    ],
       password: ['', Validators.required]
     });
   }
 
-  ngOnInit() {
-    // Any additional initialization code
-  }
+  ngOnInit() {}
 
   onSubmit() {
-    // Handle form submission
+    console.log(this.loginForm.valid);
+   if(this.loginForm.valid){
+    const userData=this.loginForm.value;
+    const DataToBeSent = {
+      email: userData.email ,
+      password: userData.password
+    };
+    console.log(DataToBeSent);
+    this.loginservies.LoginUser(DataToBeSent).subscribe(
+      (res: any) => { 
+        const token = res.token; 
+        console.log(res);
+        console.log('Token:', token); 
+    },
+    (error)=>{
+      console.log(error.error);
+    });
+    this.router.navigate(['/']);
+   }
   }
 }
 
