00e109a8a90bf838320d0075c4107c9e82ac556e
Mina Yossry <mina.yossry4@gmail.com>
2023-04-20 01:01:30 +0200

    Delete Search from navbar

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommunicationService } from 'src/app/Services/Communication/communication.service';
 import { OrderByStrings } from 'src/app/Dtos/OrderByStrings';
@@ -8,21 +8,30 @@ import { FilterService } from 'src/app/Services/Filter/filter.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   constructor(
     private NavBarService: CommunicationService,
     private router: Router,
     private readonly filterService: FilterService
   ) {}
+  ngOnInit(): void {
+    this.isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+  }
   ShowRegistrationForm() {
     this.NavBarService.toggleVisibility();
     this.router.navigate(['/registration']);
+  }
+
+  LogOut() {
+    localStorage.setItem('authToken', '');
+    localStorage.setItem('authTokenExpDate', '');
   }
 
   private OrderByStrings = OrderByStrings;
   searchText = '';
   page = 1;
   orderBy = this.OrderByStrings.PriceDesc;
+  isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
 
   searchProduct() {
     this.filterService.updateSearchText(this.searchText);
