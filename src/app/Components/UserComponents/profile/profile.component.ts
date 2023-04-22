1957830543d4ce5b992d2f484c54ef768d7a9457
Mina Yossry <mina.yossry4@gmail.com>
2023-04-22 18:44:06 +0200

    get data on load

--- src/app/Components/UserComponents/profile/profile.component.ts
+++ src/app/Components/UserComponents/profile/profile.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'jquery';
+import { UserProfileInfoDto } from 'src/app/Dtos/UserProfileInfoDto ';
+import { UserService } from 'src/app/Services/User/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  constructor(private readonly userService: UserService) {}
+
+  currentUser: UserProfileInfoDto | any;
+
+  ngOnInit(): void {
+    this.userService.GetInfo().subscribe({
+      next: (data: UserProfileInfoDto) => {
+        this.currentUser = data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
