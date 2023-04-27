30ca5d25ab7fb283362b2fe8e23642ec7879b1f1
عبدالله الحديدي <abdallah201@protonmail.com>
2023-04-27 15:00:23 +0200

    added update notification on connection start

--- src/app/Services/SignalR/signal-r.service.ts
+++ src/app/Services/SignalR/signal-r.service.ts
@@ -1,72 +1,66 @@
-import {EventEmitter, Injectable} from '@angular/core';
-import {HubConnection} from "@microsoft/signalr";
+import { EventEmitter, Injectable } from '@angular/core';
+import { HubConnection } from "@microsoft/signalr";
 import * as signalR from "@microsoft/signalr";
-import {environment} from "../../../environments/environment";
-import {MessageDto} from "../../Dtos/Message/MessageDto";
-import {MessageStatus} from "../../Dtos/Message/MessageStatus";
+import { environment } from "../../../environments/environment";
+import { MessageDto } from "../../Dtos/Message/MessageDto";
+import { MessageStatus } from "../../Dtos/Message/MessageStatus";
 
 @Injectable({
   providedIn: 'root'
 })
-export class SignalRService
-{
+export class SignalRService {
   private connection: HubConnection | null = null;
   private url = `${environment.apiURL}/chatHub`; //API
   public newMessageEvent = new EventEmitter();
   public messageSeenEvent = new EventEmitter();
   public allMessagesSeenEvent = new EventEmitter();
   public incMesCountEvent = new EventEmitter();
+  public connectionStartEvent = new EventEmitter();
   public isConnected: boolean = false;
 
-  startConnection(token?: string)
-  {
-    if (!token)
-    {
+  startConnection(token?: string) {
+    if (!token) {
       token = localStorage.getItem('authToken')?.toString() ?? '';
     }
 
-    if (!token || token == '')
-    {
+    if (!token || token == '') {
       return;
     }
 
     this.connection = new signalR.HubConnectionBuilder()
-      .withUrl(this.url, {accessTokenFactory: () => token ?? ''})
+      .withUrl(this.url, { accessTokenFactory: () => token ?? '' })
       .build();
 
     this.connection.start()
-      .then(() => this.isConnected = true)
+      .then(() => {
+        this.isConnected = true;
+        this.connectionStartEvent.emit();
+      })
       .catch((err) => console.error(err.toString()));
 
     this.connection.on("ReceiveMessage", (
-      messageId: string, message: string, senderId: string, timeStamp: string, messageStatus: MessageStatus) =>
-    {
+      messageId: string, message: string, senderId: string, timeStamp: string, messageStatus: MessageStatus) => {
       let msg = new MessageDto(messageId, message, senderId, timeStamp, messageStatus);
       this.newMessageEvent.emit(msg)
     });
 
-    this.connection.on("MessageSeen", (messageId: string) =>
-    {
+    this.connection.on("MessageSeen", (messageId: string) => {
       this.messageSeenEvent.emit(messageId)
     });
 
-    this.connection.on("AllMessagesSeen", (userId: string) =>
-    {
+    this.connection.on("AllMessagesSeen", (userId: string) => {
       this.allMessagesSeenEvent.emit(userId)
     });
   }
 
-  endConnection()
-  {
+  endConnection() {
     this.connection?.stop()
       .then(() => this.isConnected = false)
       .catch((err) => console.error(err))
   }
 
-  async sendMessage(message: string, receiverId: string, timestamp: Date)
-  {
-    if (!this.connection)
-    {
+  async sendMessage(message: string, receiverId: string, timestamp: Date) {
+    if (!this.connection) {
       console.error("SignalR connection isn't established")
       return ''
     }
@@ -80,16 +74,13 @@ export class SignalRService
     return _messageId
   }
 
-  setMessageSeen(messageId: string, senderId: string)
-  {
-    if (!this.connection)
-    {
+  setMessageSeen(messageId: string, senderId: string) {
+    if (!this.connection) {
       return console.error("SignalR connection isn't established")
     }
 
     this.connection.invoke("SetMessageSeen", messageId, senderId)
-      .then(() =>
-      {})
+      .then(() => { })
       .catch((err) => console.error("Message couldn't be sent", err));
   }
 
