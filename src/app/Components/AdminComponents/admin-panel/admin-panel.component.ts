8db00a43d8fe2f1319208f52b19d39bf05848c2b
Mina Yossry <mina.yossry4@gmail.com>
2023-05-06 23:52:27 +0300

    fix admin authorize

--- src/app/Components/AdminComponents/admin-panel/admin-panel.component.ts
+++ src/app/Components/AdminComponents/admin-panel/admin-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/Services/Login/login.service';
 import { SignalRService } from 'src/app/Services/SignalR/signal-r.service';
@@ -8,12 +8,18 @@ import { SignalRService } from 'src/app/Services/SignalR/signal-r.service';
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.css'],
 })
-export class AdminPanelComponent {
+export class AdminPanelComponent implements OnInit {
   selected: string = 'reports';
-  constructor(private readonly loginService: LoginService, private readonly router: Router, private readonly signalRService: SignalRService) {
-
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly router: Router,
+    private readonly signalRService: SignalRService
+  ) {}
+  ngOnInit(): void {
+    this.userRole = localStorage.getItem('userRole') ?? '';
   }
 
+  userRole?: string = localStorage.getItem('userRole') ?? '';
   updateComponent(view: string) {
     this.selected = view;
   }
