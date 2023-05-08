866fa7234a04f2dee52f1b0245c20d03201d35fc
عبدالله الحديدي <abdallah201@protonmail.com>
2023-05-08 14:49:04 +0300

    added feed back to user

--- src/app/Components/Checkout/payment/payment.component.ts
+++ src/app/Components/Checkout/payment/payment.component.ts
@@ -35,7 +35,7 @@ export class PaymentComponent {
     private http: HttpClient,
     private fb: FormBuilder,
     private stripeService: StripeService,
-    private router : Router
+    private router: Router
   ) {
 
 
@@ -44,48 +44,49 @@ export class PaymentComponent {
   }
 
   ngOnInit() {
-    this.createPaymentIntent(2500 )
+    this.createPaymentIntent(2500)
       .subscribe(pi => {
-        console.log( "hmm",pi?.client_secret);
+        console.log("hmm", pi?.client_secret);
         this.elementsOptions.clientSecret = pi?.client_secret ?? "";
-      
+
       });
   }
 
   pay() {
-      this.paying = true;
-      this.stripeService.confirmPayment({
-        elements: this.paymentElement.elements,
-        // confirmParams: {
-        //   payment_method_data: {
-        //     billing_details: {
-        //       name: this.paymentElementForm.get('name')?.value,
-        //       email: this.paymentElementForm.get('email')?.value,
-        //       address: {
-        //         line1: this.paymentElementForm.get('address')?.value,
-        //         postal_code: this.paymentElementForm.get('zipcode')?.value,
-        //         city: this.paymentElementForm.get('city')?.value,
-        //       }
-        //     }
-        //   }
-        // },
-        redirect: 'if_required'
-      }).subscribe(result => {
-        console.log("Confirmed");
-        this.paying = false;
-        if (result.error) {
-          console.log('Result', result.error);
-          // Show error to your customer (e.g., insufficient funds)
-          alert(result.error.code);
-        } else {
-          console.log('Result', result.paymentIntent);
-          // The payment has been processed!
-          if (result.paymentIntent.status === 'succeeded') {
-            // Show a success message to your customer
-            this.router.navigateByUrl('/');
-          }
+    this.paying = true;
+    this.stripeService.confirmPayment({
+      elements: this.paymentElement.elements,
+      // confirmParams: {
+      //   payment_method_data: {
+      //     billing_details: {
+      //       name: this.paymentElementForm.get('name')?.value,
+      //       email: this.paymentElementForm.get('email')?.value,
+      //       address: {
+      //         line1: this.paymentElementForm.get('address')?.value,
+      //         postal_code: this.paymentElementForm.get('zipcode')?.value,
+      //         city: this.paymentElementForm.get('city')?.value,
+      //       }
+      //     }
+      //   }
+      // },
+      redirect: 'if_required'
+    }).subscribe(result => {
+      console.log("Confirmed");
+      this.paying = false;
+      if (result.error) {
+        console.log('Result', result.error);
+        // Show error to your customer (e.g., insufficient funds)
+        alert(result.error.code);
+      } else {
+        console.log('Result', result.paymentIntent);
+        // The payment has been processed!
+        if (result.paymentIntent.status === 'succeeded') {
+          // Show a success message to your customer
+          alert("Payment Succeeded");
+          this.router.navigateByUrl('/');
         }
-      });
+      }
+    });
 
   }
 
